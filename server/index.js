0589dcc5f1fd9e0b58ace260842451b2a430e9c5
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-24 17:48:43 -0500

    implement route for get request with id param

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 const PORT = 3002;
-
-app.get('/', (req, res) => {
-  res.send('Booking!');
+//TEST
+// app.get('/', (req, res) => {
+//   res.send('Booking!');
+// })
+//BOOKING DATA REQUEST
+//BROKEN
+app.get('/:id', (req, res) => {
+  var site = req.params;
+  console.log(site, "<-----siteid")
+  //quere db collection where site id === site.id
+  res.send('hi!')
 })
 
 app.listen(PORT, ()=> {

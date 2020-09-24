d9c44f7a0222241be22338dcfacbeb1862045de6
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-24 15:49:35 -0500

    install express and set up basic server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3002;
 
+app.get('/', (req, res) => {
+  res.send('Booking!');
+})
+
+app.listen(PORT, ()=> {
+  console.log(`listening on ${PORT}`);
+})

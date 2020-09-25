4c5d825d273e88d602436df34cde9ea9be02ed50
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-24 21:46:39 -0500

    separate concerns-- seed file, site model, db connection

--- db/Site.js
+++ db/Site.js
@@ -1,18 +1,19 @@
-// const mongoose = require('mongoose');
-// const db = require('./index.js');
+ const mongoose = require('mongoose');
+ const db = require('./index.js');
+mongoose.Promise = global.Promise;
 
 // const dateSchema = new mongoose.Schema({
 //   date: Date,
 //   available: Boolean
 // })
-// const siteSchema = new mongoose.Schema({
-//   siteId: Number,
-//   price: Number,
-//   weekdayDisc: Number,
-//   //availability: Array,
-//   minStay: {type: Number, default: 2},
-//   maxGuests: {type: Number, default: 6},
-// })
+const siteSchema = new mongoose.Schema({
+  siteId: Number,
+  price: Number,
+  weekdayDisc: Number,
+  availability: Array,
+  minStay: {type: Number, default: 2},
+  maxGuests: {type: Number, default: 6},
+})
 
-// const Site = mongoose.model('Site', siteSchema);
-// module.exports = Site;
+ const Site = mongoose.model('Site', siteSchema);
+ module.exports = Site;

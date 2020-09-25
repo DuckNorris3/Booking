fa83124d09cb78fa137b6e5040a60a9a074766cc
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-24 19:24:38 -0500

    connect database and insert seed data

---
+++ db/Site.js
@@ -0,0 +1,18 @@
+// const mongoose = require('mongoose');
+// const db = require('./index.js');
+
+// const dateSchema = new mongoose.Schema({
+//   date: Date,
+//   available: Boolean
+// })
+// const siteSchema = new mongoose.Schema({
+//   siteId: Number,
+//   price: Number,
+//   weekdayDisc: Number,
+//   //availability: Array,
+//   minStay: {type: Number, default: 2},
+//   maxGuests: {type: Number, default: 6},
+// })
+
+// const Site = mongoose.model('Site', siteSchema);
+// module.exports = Site;

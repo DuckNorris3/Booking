a5e75a97c834a568fc39ca3c7ac3171dcbc944ca
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-28 14:07:37 -0500

    add skeleton components to page

---
+++ public/src/components/Guests.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Guests = ({maxGuests}) => {
+  return (
+    <div>
+      <button>-</button><label>Guests</label><button>+</button>
+    </div>
+  )
+}
+
+export default Guests;
---
+++ public/src/components/Price.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Price = ({price}) => {
+  return(
+    <div>${price}</div>
+  )
+
+}
+
+export default Price;

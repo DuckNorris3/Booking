d1a20bf4e226e4f197cf80b7313af715c43bd8ed
Claire Melbourne <claire.melbourne@gmail.com>
2020-09-28 16:43:25 -0500

    add db disconnect to seed function

--- db/seedData.js
+++ db/seedData.js
@@ -1,8 +1,20 @@
 const db = require('./index.js');
 const Site = require('./Site');
 const seed = require('./mockData.js');
+const mongoose = require('mongoose');
 
 const addSampleData = function() {
+  //drop db
   Site.create(seed.data)
-}
+  .then(() => {
+    console.log("db SEEDED ALRIGHT");
+    mongoose.connection.close(() => {
+      console.log("Mongoose disconnected!")
+    });
+  })
+  .catch((err) => {
+    console.log("here's the problem: ", err);
+  });
+};
+  //close connection
 addSampleData();

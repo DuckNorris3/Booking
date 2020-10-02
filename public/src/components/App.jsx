72b00caa4ff7bcb9536c30b705144562357fb54f
Claire Melbourne <claire.melbourne@gmail.com>
2020-10-02 16:15:32 -0500

    add checkin checkout selected highlight

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -32,17 +32,8 @@ const FlexRow = styled.div`
   margin-left: -10px;
   margin-right: -10px;
 `;
-const SelectDateCol = styled.div`
-  padding: 10px;
-  border-right: 1px solid #ebebeb;
-  &:hover {
-    background-color: lightgrey;
-  }
-`;
-const Col = styled.div`
-  padding: 10px;
-  border-right: 1px solid #ebebeb;
-`;
+
+
 
 function App() {
   const[siteData, setSiteData] = useState(null);
@@ -54,7 +45,22 @@ function App() {
   const[nights, setNights] = useState(0);
   const[discount, setDiscount] = useState(0)
   const[totals, setTotals] = useState(0);
-
+  const Col = styled.div`
+  padding: 10px;
+  border-right: 1px solid #ebebeb;
+  &.checkIn {
+    &:hover {
+      background-color: ${checkInSelect ? 'grey' : 'lightgrey'}
+    }
+    background-color: ${checkInSelect ? 'lightgrey' : 'white'}
+  }
+  &.checkOut {
+    &:hover {
+      background-color: ${checkOutSelect ? 'grey' : 'lightgrey'}
+    }
+    background-color: ${checkOutSelect ? 'lightgrey' : 'white'}
+  }
+`;
   useEffect(() => {
     axios.get(`/5`)
       .then((result) => {
@@ -177,12 +183,12 @@ function App() {
         </div>
           <div className="well-content">
             <FlexRow>
-              <SelectDateCol>
+              <Col className="checkIn">
                 <CheckIn checkIn= {checkIn} handleClick= { () => selectCheckIn() } showCalendar= {showCalendar} checkInSelect= {checkInSelect} />
-              </SelectDateCol>
-              <SelectDateCol>
+              </Col>
+              <Col className= "checkOut">
                 <CheckOut checkOut= {checkOut} handleClick= { () => selectCheckOut() } showCalendar= {showCalendar}/>
-              </SelectDateCol>
+              </Col>
               <Col>
                 <Guests maxGuests= {siteData.maxGuests}/>
               </Col>

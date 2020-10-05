c31e813d690775507f709fbc568fe44ac47c5086
Claire Melbourne <claire.melbourne@gmail.com>
2020-10-05 13:55:02 -0500

    minor expansion on seed testing and front end components

--- spec/mockData.test.js
+++ spec/mockData.test.js
@@ -4,3 +4,7 @@ test('generates an array of objects', () => {
   expect(Array.isArray(seedData.data)).toBe(true);
 });
 
+test('data entry contains siteId', () => {
+  expect(seedData.data[0].siteId).toBe(1);
+});
+
